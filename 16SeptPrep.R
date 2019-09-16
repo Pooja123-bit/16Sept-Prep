9c26a79e4f882d486dbe7fddd10d702caf137aad
Pooja123-bit <poojaupadhyay100@gmail.com>
2019-09-16 15:25:46 -0500

    edit

---
+++ 16SeptPrep.R
@@ -0,0 +1,29 @@
+list.files(pattern=".txt")
+list.files()
+d<-read.table(file= 'ISIIS201405291242.txt',
+              sep="\t", skip=10, header=TRUE, fileEncoding="ISO-8859-1",
+              stringsAsFactors=FALSE ,quote="\"", check.names=FALSE,
+              encoding="UTF-8", na.strings="9999.99")
+head(d)
+
+#create a proper date + time format
+date<-scan(file= 'ISIIS201405291242.txt',
+           what="character", skip=1, nlines=1, quiet=TRUE)
+
+date
+date<-date[2]
+date
+
+library("stringr")
+mm<- str_sub(string=date,start=1,end=2)
+mm
+
+dd<- str_sub(date,4,5)
+dd
+
+dd<- as.numeric(dd)
+dd
+
+yy<-str_sub(date,7,8)
+yy
+
